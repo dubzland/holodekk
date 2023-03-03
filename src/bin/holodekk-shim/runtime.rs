0ae732d3b808824eb7cbb496fc6c0f8b49c0225f
Josh Williams <jdubz@holodekk.io>
2023-03-03 15:36:20 +0000

    finished major refactoring.  moving on to runtime.

--- src/bin/holodekk-shim/runtime.rs
+++ src/bin/holodekk-shim/runtime.rs
@@ -4,11 +4,13 @@ use std::path::{Path, PathBuf};
 
 use log::debug;
 
+use libsee::Pid;
+
 use holodekk::libsee;
 use holodekk::streams::{create_pipes, override_streams};
 
 pub trait Command {
-    fn to_argv(&self, runtime: &str, id: &str) -> Vec<CString>;
+    fn to_argv(&self, runtime: &str, pidfile: &str, id: &str) -> Vec<CString>;
 }
 
 pub struct CreateCommand {
@@ -24,11 +26,13 @@ impl CreateCommand {
 }
 
 impl Command for CreateCommand {
-    fn to_argv(&self, runtime: &str, id: &str) -> Vec<CString> {
+    fn to_argv(&self, runtime: &str, pidfile: &str, id: &str) -> Vec<CString> {
         vec![
             CString::new(runtime).unwrap(),
             CString::new("create").unwrap(),
-            CString::new("-b").unwrap(),
+            CString::new("--pid-file").unwrap(),
+            CString::new(pidfile).unwrap(),
+            CString::new("--bundle").unwrap(),
             CString::new(self.bundle_path.display().to_string()).unwrap(),
             CString::new(id).unwrap()
         ]
@@ -36,18 +40,25 @@ impl Command for CreateCommand {
 }
 
 pub struct ExecCommand {
-    pidfile: PathBuf,
     arguments: Vec<String>
 }
 
+impl ExecCommand {
+    pub fn new(args: &Vec<String>) -> Self {
+        Self {
+            arguments: args.to_owned()
+        }
+    }
+}
+
 impl Command for ExecCommand {
-    fn to_argv(&self, runtime: &str, id: &str) -> Vec<CString> {
+    fn to_argv(&self, runtime: &str, pidfile: &str, id: &str) -> Vec<CString> {
         let mut argv = vec![
             CString::new(runtime).unwrap(),
             CString::new("exec").unwrap(),
             CString::new("--detach").unwrap(),
             CString::new("--pid-file").unwrap(),
-            CString::new(self.pidfile.display().to_string()).unwrap(),
+            CString::new(pidfile).unwrap(),
             CString::new(id).unwrap()
         ];
 
@@ -58,71 +69,58 @@ impl Command for ExecCommand {
     }
 }
 
-impl ExecCommand {
-    pub fn new<F: AsRef<Path>>(pidfile: F, args: &Vec<String>) -> Self {
-        Self {
+pub struct Container {
+    id: String,
+    pidfile: PathBuf,
+    pid: Pid,
+}
+
+impl Container {
+    pub fn new<F: AsRef<Path>>(id: &str, pidfile: F) -> Self {
+        Container {
+            id: id.to_string(),
             pidfile: pidfile.as_ref().to_owned(),
-            arguments: args.to_owned()
+            pid: -1
         }
     }
 }
 
-pub struct Shim {
-    runtime_path: PathBuf,
-    container_id: String,
-}
+pub fn exec<F: AsRef<Path>>(runtime_path: F, container: &mut Container, cmd: Box<dyn Command>) {
+    override_streams((None, None, None)).unwrap();
+    libsee::setsid().unwrap();
+    libsee::prctl(libc::PR_SET_CHILD_SUBREAPER, 1, 0, 0, 0).unwrap();
 
-impl Shim {
-    pub fn new<F: AsRef<Path>>(runtime_path: F) -> Self {
-        Shim {
-            runtime_path: runtime_path.as_ref().to_owned(),
-            container_id: "".to_string(),
-        }
-    }
+    let (iomain, ioworker) = create_pipes(false, true, true);
 
-    pub fn container_id(mut self, id: &str) -> Self {
-        self.container_id = id.to_owned();
-        self
-    }
+    let res = libsee::fork().expect("unable to fork runtime process.");
+    if let Some(pid) = res {
+        debug!("runtime forked with pid {}", pid);
+        drop(ioworker);
+        let mut status: libc::c_int = -1;
+        libsee::waitpid(pid, &mut status, 0).unwrap();
 
-    pub fn exec(&self, cmd: Box<dyn Command>) -> Option<libsee::Pid> {
-        let res = libsee::fork().unwrap();
-        if let Some(pid) = res {
-            return Some(pid);
-        }
-
-        override_streams((None, None, None)).unwrap();
-        libsee::setsid().unwrap();
-        libsee::prctl(libc::PR_SET_CHILD_SUBREAPER, 1, 0, 0, 0).unwrap();
-
-        let (iomain, ioworker) = create_pipes(false, true, true);
-
-        let res = libsee::fork().expect("unable to fork runtime process.");
-        if let Some(pid) = res {
-            drop(ioworker);
-            let mut status: libc::c_int = -1;
-            libsee::waitpid(pid, &mut status, 0).unwrap();
-
-            if !libc::WIFEXITED(status) || libc::WEXITSTATUS(status) != 0 {
-                debug!("worker failed: {}", libc::WEXITSTATUS(status));
-            } else {
-                debug!("worker exited with status: {}", libc::WEXITSTATUS(status));
-            }
+        if !libc::WIFEXITED(status) || libc::WEXITSTATUS(status) != 0 {
+            debug!("runtime failed: {}", libc::WEXITSTATUS(status));
             let mut buf = Vec::new();
             let line = &mut buf;
             if let (_, _, Some(mut stderr)) = iomain.streams() {
                 if let Err(err) = stderr.read_to_end(line) {
-                    debug!("[shim] failed to read runtime's STDERR: {}", err);
+                    debug!("failed to read runtime's STDERR: {}", err);
                 }
             }
             debug!("{}", String::from_utf8(line.to_vec()).unwrap_or(format!("{:?}", buf)));
         } else {
-            drop(iomain);
-            override_streams(ioworker.streams()).expect("Unable to override streams");
-            let argv = cmd.to_argv(self.runtime_path.to_str().unwrap(), &self.container_id);
-            libsee::execv(&argv);
+            debug!("runtime exited with status: {}", libc::WEXITSTATUS(status));
         }
-        None
+    } else {
+        drop(iomain);
+        override_streams(ioworker.streams()).expect("Unable to override streams");
+        let argv = cmd.to_argv(
+            runtime_path.as_ref().to_str().unwrap(),
+            container.pidfile.to_str().unwrap(),
+            &container.id
+        );
+        libsee::execv(&argv);
+        libsee::_exit(127);
     }
-
 }
