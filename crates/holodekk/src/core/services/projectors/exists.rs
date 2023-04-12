1916488c3c8ecbdd6cafc2d1839a867a124fe15b
Josh Williams <jdubz@holodekk.io>
2023-04-12 22:49:05 +0000

    Add tests to ProjectorsService.exists()

--- crates/holodekk/src/core/services/projectors/exists.rs
+++ crates/holodekk/src/core/services/projectors/exists.rs
@@ -27,3 +27,69 @@ where
         self.repo.projector_exists(&id).await
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use rstest::*;
+
+    use crate::config::fixtures::{mock_config, MockConfig};
+    use crate::core::repositories::{fixtures::projector_repository, MockProjectorRepository};
+
+    use super::*;
+
+    #[rstest]
+    #[tokio::test]
+    async fn returns_true_for_existing_projector(
+        mock_config: MockConfig,
+        mut projector_repository: MockProjectorRepository,
+    ) {
+        let (cmd_tx, _cmd_rx) = tokio::sync::mpsc::channel(1);
+
+        projector_repository
+            .expect_projector_exists()
+            .return_const(true);
+
+        let service = ProjectorsService::new(
+            Arc::new(mock_config),
+            Arc::new(projector_repository),
+            cmd_tx,
+        );
+
+        assert!(
+            service
+                .exists(ProjectorExistsInput {
+                    namespace: "existing".to_string()
+                })
+                .await
+        );
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn returns_false_for_nonexisting_projector(
+        mock_config: MockConfig,
+        mut projector_repository: MockProjectorRepository,
+    ) {
+        let (cmd_tx, _cmd_rx) = tokio::sync::mpsc::channel(1);
+
+        projector_repository
+            .expect_projector_exists()
+            .return_const(false);
+
+        let service = ProjectorsService::new(
+            Arc::new(mock_config),
+            Arc::new(projector_repository),
+            cmd_tx,
+        );
+
+        assert!(
+            !service
+                .exists(ProjectorExistsInput {
+                    namespace: "existing".to_string()
+                })
+                .await
+        );
+    }
+}
